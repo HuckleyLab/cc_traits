3a5639a3635bea4c1c76ee903d63b189c58bd113
Tony Cannistra <tony.cannistra@gmail.com>
2018-12-06 11:22:38 -0800

    updated plot organization

--- code/summarize_mse.py
+++ code/summarize_mse.py
@@ -13,14 +13,16 @@ def get_dataset_mses(dsname, rootdir):
 
     datafile = glob(path.join(datapath, "*mseraw.csv"))[0]
     return(pd.read_csv(datafile, converters={'MSEs': literal_eval}))
- 
+
 
 def improvement(mses, over = 'OLS'):
     OLS = np.mean(mses.MSEs[mses.method == over].iloc[0])
-    improvement = np.divide(np.array([np.mean(x)
-                            for x in mses.MSEs.values] - OLS),
-                            OLS) * 100
-    mses['improvement'] = improvement
+    improvement_raw = np.array([np.mean(x) for x in mses.MSEs.values]) - OLS
+
+    improvement_pct = np.divide(improvement_raw, OLS) * 100
+
+    mses['improvement_pct'] = improvement_pct
+    mses['improvement_raw'] = improvement_raw
     return(mses)
 
 
@@ -31,10 +33,22 @@ def main():
     print("found datasets: {}".format(
         ", ".join([d['name'].replace(" ", "-") for d in dsinfo])))
 
+
+    # just one dataset?
+    dsname = None
+    try:
+        dsname = argv[3]
+        print("using dataset \"{}\"".format(dsname))
+    except:
+        print("using all datasets")
+
     # look for dataset mses files
 
     files = []
     for ds in dsinfo:
+        if dsname != None and ds['name'] != dsname:
+            # skip all that aren't focal ds
+            continue;
         try:
             files.append(get_dataset_mses(ds['name'], argv[2]))
         except Exception:
@@ -44,7 +58,7 @@ def main():
     # compute improvement over OLS over all methods
     improvements = []
     for file in files:
-        try: 
+        try:
             improvements.append(improvement(file))
         except:
             continue
@@ -53,8 +67,9 @@ def main():
     # compute average improvement across datasets for all methods:
     methods = improvements.method.unique()
     for method in methods:
-        avg_improvement = np.mean(improvements.improvement[improvements.method == method].values)
-        print("average improvement of {} over OLS is {}".format(method, avg_improvement))
+        avg_improvement_pct = np.mean(improvements.improvement_pct[improvements.method == method].values)
+        avg_improvement_raw = np.mean(improvements.improvement_raw[improvements.method == method].values)
+        print("average improvement of {} over OLS is {}% (raw: {})".format(method, avg_improvement_pct, avg_improvement_raw))
 
 
 
